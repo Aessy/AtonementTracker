d8a343a2ef19787c967919e28fc548f8a8e9a2b0
Jens Haraldset <jens.haraldsete@gmail.com>
2016-07-23 23:30:12 +0000

    Now tracks currently active atonements with a timer.

--- atonement_tracker.lua
+++ atonement_tracker.lua
@@ -8,7 +8,41 @@ function AtonementTracker:Create()
     setmetatable(tracker, AtonementTracker)
 
     tracker.f = CreateFrame("FRAME", nil, UIParent)
+
+    local backdrop = {
+      -- path to the background texture
+      bgFile = "Interface\\DialogFrame\\UI-DialogBox-Gold-Background",  
+      -- true to repeat the background texture to fill the frame, false to scale it
+      tile = true,
+      -- size (width or height) of the square repeating background tiles (in pixels)
+      tileSize = 32,
+    }
+
+    tracker.f:SetBackdrop(backdrop)
+
+
     tracker.atonements = {}
+    tracker.active = 0
+
+    tracker.atonement_labels = {}
+    tracker.active_labels = 0
+
+    local last_lbl = nil
+    for i=0,10 do
+        lbl = tracker.f:CreateFontString(nil, "ARTWORK", "ChatFontNormal")
+        lbl:SetText(i)
+        lbl:Hide()
+        if last_lbl == nil then
+            lbl:SetPoint("TOPLEFT", tracker.f, "TOPLEFT")
+        else
+            lbl:SetPoint("TOPLEFT", last_lbl, "BOTTOMLEFT")
+        end
+        atn = {}
+        atn.lbl = lbl
+        atn.active = false
+        tracker.atonement_labels[i] = atn
+        last_lbl = lbl
+    end
 
     tracker.f:SetMovable(true)
 
@@ -23,7 +57,7 @@ function AtonementTracker:Create()
     end)
 
     tracker.f:SetWidth(110)
-    tracker.f:SetHeight(30)
+    tracker.f:SetHeight(200)
     tracker.f:SetPoint("CENTER", UIParent, "CENTER")
     tracker.f:Show()
 
@@ -32,18 +66,70 @@ end
 
 function AtonementTracker:Apply(src, dst)
     print(dst)
-    str = self.f:CreateFontString(nil, "ARTWORK", "ChatFontNormal")
-    str:SetText(dst)
-    str:SetPoint("CENTER", self.f, "CENTER")
-    str:Show()
 
-    self.atonements[dst] = str
+    atonement = {}
+    atonement.timer = 15
+    atonement.target = dst
+    self.atonements[dst] = atonement
+    
+    self.active = self.active + 1
+
+    self:Reorganize()
+end
+
+function AtonementTracker:GetSortedAtonements()
+    sorted = {}
+    for k, n in pairs(self.atonements) do table.insert(sorted, n) end
+    table.sort(sorted, function(a, b) return a.timer < b.timer end)
+    return sorted
+end
+
+function AtonementTracker:Reorganize()
+
+    sorted = self:GetSortedAtonements()
+
+    local atonement = 1
+    for k, v in ipairs(self.atonement_labels) do
+        v.lbl:Hide()
+        if atonement < self.active + 1 then
+            v.atonement = sorted[atonement]
+            v.valid = true
+            atonement = atonement + 1
+        end
+    end
+
+    self:UpdateGui()
+end
+
+function AtonementTracker:UpdateGui()
+    local atonement = 1
+    for k, v in ipairs(self.atonement_labels) do
+        if atonement < self.active + 1 then
+            v.lbl:SetText((self.active - atonement + 1)..": "..string.format("%.1f",v.atonement.timer))
+            if atonement == 1 then
+                v.lbl:SetTextColor(1,0,0)
+            end
+            v.lbl:Show()
+        end
+        atonement = atonement + 1
+    end
 end
 
 function AtonementTracker:Remove(src, dst)
     print(dst)
-    self.atonements[dst]:Hide()
-    self.atonements[dst] = nil
+    if self.atonements[dst] ~= nil then
+        self.atonements[dst] = nil
+        self.active = self.active - 1
+        self:Reorganize()
+    end
+end
+
+function AtonementTracker:Refresh(src, dst)
+    print(dst)
+    if self.atonements[dst] ~= nil then
+        self.atonements[dst].timer = 15
+        self:Reorganize()
+    end
 end
 
 function AtonementTracker:Event(eventtype, src_name, dst_name, spell_id, spell_name, spell_school, aura_type)
@@ -55,10 +141,30 @@ function AtonementTracker:Event(eventtype, src_name, dst_name, spell_id, spell_n
             self:Apply(src, dst)
         elseif eventtype == "SPELL_AURA_REFRESH" then
             print("Refreshed: ["..spell_name.."]")
+            self:Refresh(src, dst)
         elseif eventtype == "SPELL_AURA_REMOVED" then
             print("Removed: ["..spell_name.."]")
             self:Remove(src, dst)
         end
+    end
+end
+
+function AtonementTracker:Tick(elapsed)
+    local removed = false
+    for k, v in pairs(self.atonements) do
+        v.timer = v.timer - elapsed
+        if v.timer <= 0 then
+            v.active = false
+            self.atonements[k] = nil
+            self.active = self.active - 1
+            removed = true
+        end
+    end
+
+    if removed == true then
+        self:Reorganize()
+    else
+        self:UpdateGui()
     end
 end
 
@@ -68,4 +174,8 @@ local event_handler = CreateFrame("Frame")
 event_handler:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 event_handler:SetScript("OnEvent", function(frame, event, timestamp, eventtype, hideCaster, srcGUID, srcName, srcFlags, srcRaidFlags, dstGUID, dstName, dstFlags, dstRaidFlags, ...)
     current_window:Event(eventtype, srcName, dstName, ...)
+end)
+
+event_handler:SetScript("OnUpdate", function(self, elapsed)
+    current_window:Tick(elapsed)
 end)
